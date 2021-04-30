6ed6e1272a40c3d69140b5d66a5531d73f501570
Sam Nagle <samnagle@users.noreply.github.com>
2021-04-30 11:12:01 -0700

    New version find a doc

--- df_custom_components/components/find_a_doctor/find_a_doctor.js
+++ df_custom_components/components/find_a_doctor/find_a_doctor.js
@@ -2,155 +2,78 @@ const Switch = ReactRouterDOM.Switch;
 const Route = ReactRouterDOM.Route;
 const Link = ReactRouterDOM.Link;
 
-const InlineForm = () => {
+
+
+const FormOne = () => {
   return (
-    <div className="container">
-      <form className="form-inline">
-        <div className="form-group mb-2">
-          <label htmlFor="inputState">I'm a </label>
-          <select id="inputState" className="form-control">
-            <option selected>Patient</option>
-          </select>
-        </div>
-        <div className="form-group mb-2">
-          <label htmlFor="inputState">and I want to </label>
-          <select id="inputState" className="form-control">
-            <option selected>Find Doctor or other Provider</option>
-          </select>
-        </div>
-        <button type="submit" className="btn btn-lg mb-2">
-          <Link to="/speciality">Go</Link>
-        </button>
-      </form>
+    <div className="form-container">
+      <div className="form-wrapper">
+        <form className="form-inline">
+          <div className="form-group mb-2">
+            <label htmlFor="inputState">I'm a </label>
+            <select id="inputState" className="form-control" defaultValue={0}>
+              <option value={0}>Patient</option>
+            </select>
+          </div>
+          <div className="form-group mb-2">
+            <label htmlFor="inputState">and I want to </label>
+            <select id="inputState" className="form-control" defaultValue={0}>
+              <option value={0}>Find Doctor or other Provider</option>
+            </select>
+          </div>
+          <Link to="/speciality">
+            <button type="submit" className="btn btn-lg mb-2">
+              Go
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
 
-const InlineForm1 = () => {
+
+const FormTwo = () => {
   return (
-    <div className="container">
-      <form className="form-inline">
-        <div className="form-group mb-2">
-          <label htmlFor="inputState">Speciality </label>
-          <select id="inputState" className="form-control">
-            <option selected disabled>
-              Patient
-            </option>
-            <option>Cardiology</option>
-            <option>Dermatology</option>
-            <option>Neurology</option>
-          </select>
-        </div>
-        <div className="form-group mb-2">
-          <label htmlFor="inputState">Zip Code</label>
-          <input type="text" className="form-control" placeholder="Zip Code" />
-        </div>
-        <Link to="/recommendation">Go</Link>
-      </form>
+    <div className="form-container">
+      <div className="form-wrapper">
+        <form className="form-inline">
+          <div className="form-group mb-2">
+            <label htmlFor="inputState">Speciality </label>
+            <select id="inputState" className="form-control" defaultValue={0}>
+              <option value={0}>Patient</option>
+              <option value={1}>Cardiology</option>
+              <option value={2}>Dermatology</option>
+              <option value={3}>Neurology</option>
+            </select>
+          </div>
+          <div className="form-group mb-2">
+            <label htmlFor="inputState">Zip Code</label>
+            <input type="text" className="form-control" placeholder="Zip Code"/>
+          </div>
+          <Link to="/recommendation">
+            <button type="submit" className="btn btn-lg mb-2">Go
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
 
-const Main = () => {
-  return (
-    <section className="hero-section">
-      <div className="hero-image">
-        <div className="container">
-          <form className="form-inline">
-            <div className="form-group mb-2">
-              <label htmlFor="inputState">I'm a </label>
-              <select
-                id="inputState"
-                className="form-control"
-                defaultValue="default"
-              >
-                <option value="default">Patient</option>
-              </select>
-            </div>
-            <div className="form-group mb-2">
-              <label htmlFor="inputState">and I want to </label>
-              <select
-                id="inputState"
-                className="form-control"
-                defaultValue="default"
-              >
-                <option value="default">Find Doctor or other Provider</option>
-              </select>
-            </div>
-            <div
-              style={{
-                backgroundColor: "blue",
-                padding: "10px 30px"
-              }}
-            >
-              <Link style={{ color: "white" }} to="/speciality">
-                Go
-              </Link>
-            </div>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-const Main1 = () => {
-  return (
-    <section className="hero-section">
-      <div className="hero-image">
-        <div className="container">
-          <form className="form-inline">
-            <div className="form-group mb-2">
-              <label htmlFor="inputState">Speciality </label>
-              <select id="inputState" className="form-control" defaultValue="">
-                <option value="" disabled>
-                  Speciality
-                </option>
-                <option>Cardiology</option>
-                <option>Dermatology</option>
-                <option>Neurology</option>
-              </select>
-            </div>
-            <div className="form-group mb-2">
-              <label htmlFor="inputState">Zip Code</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Zip Code"
-              />
-            </div>
-            {/* <button type="submit" className="btn btn-lg mb-2"> */}
-            <div
-              style={{
-                backgroundColor: "blue",
-                padding: "10px 30px"
-              }}
-            >
-              <Link style={{ color: "white" }} to="/recommendation">
-                Go
-              </Link>
-            </div>
-            {/* </button> */}
-          </form>
-        </div>
-      </div>
-    </section>
-  );
-};
 
 const Recommendation = () => {
-  const doctor =
-    "https://www.gundersenhealth.org/app/files/public/f3662785-7754-4ccf-87ab-2795c9883308/Abidi-Mateen.jpg?size=large";
-  const doctor2 = "https://www.gundersenhealth.org/app/files/public/ailiani-raju.jpg?size=large";
-  const doctor3 = "https://www.gundersenhealth.org/app/files/public/Benkers-Nicky.jpg?size=large"
+  const doctor = 'https://www.gundersenhealth.org/app/files/public/f3662785-7754-4ccf-87ab-2795c9883308/Abidi-Mateen.jpg?size=large';
 
   return (
     <section className="recommended-slider">
       <div className="container">
-        <h4 className="card-title">Doctors Recommended For You</h4>
+        <h2 className="card-title font-weight-bold">Recommended For You
+        </h2>
         <div
           id="carouselExampleControls"
           className="carousel slide"
+          data-interval="false"
           data-ride="carousel"
         >
           <div className="carousel-inner">
@@ -159,33 +82,35 @@ const Recommendation = () => {
                 <div className="carousel-item active">
                   <div className="card">
                     <div className="card-body">
-                      <div className="card-1">
+                      <div className="card-body-inner">
                         <div className="content">
                           <div className="left">
                             <div className="left-img">
-                              <img src={doctor} width="126px" alt="" />
+                              <img src={doctor} width="126px" alt=""/>
                             </div>
                             <div className="left-text">
                               <h3>
-                                <b>Mateen F. Abidi, MD, FACC, FASE</b>
+                                <b>Alpha</b>
                               </h3>
-                              <p>Cardiology</p>
-                              <b>La Crosse Campus</b>
-                              <p> La Crosse, WI</p>
+                              <p>Child Specialist</p>
+                              <h3>
+                                <b>Beta Company</b>
+                              </h3>
+                              <p> Pakistan</p>
                             </div>
                           </div>
                           <div className="right">
-                            <p>(608) 775-2335</p>
-                            <p className="greenText">Make an appointment</p>
+                            <p>+1232123432</p>
+                            <p className="greenText">Make appointment</p>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <p className="p-1">
-                              <small>5 out of 5</small>{" "}
+                              <small>5 out of 5</small>{' '}
                             </p>
-                            <p className="greenText">Patient Ratings</p>
+                            <p className="greenText">Patient list</p>
                             <p className="greenText">Comments</p>
                           </div>
                         </div>
@@ -196,23 +121,25 @@ const Recommendation = () => {
                 <div className="carousel-item">
                   <div className="card">
                     <div className="card-body">
-                      <div className="card-1">
+                      <div className="card-body-inner">
                         <div className="content">
                           <div className="left">
                             <div className="left-img">
-                              <img src={doctor2} width="126px" alt="" />
+                              <img src={doctor} width="126px" alt=""/>
                             </div>
                             <div className="left-text">
                               <h3>
-                                <b>Raju G. Ailiani, MD</b>
+                                <b>Alpha</b>
                               </h3>
-                              <p>Cardiology</p>
-                              <b>La Crosse Campus</b>
-                              <p> La Crosse, WI</p>
+                              <p>Child Specialist</p>
+                              <h3>
+                                <b>Beta Company</b>
+                              </h3>
+                              <p> Pakistan</p>
                             </div>
                           </div>
                           <div className="right">
-                            <p>(608) 775-2335</p>
+                            <p>+1232123432</p>
                             <p className="greenText">Make appointment</p>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
@@ -220,9 +147,9 @@ const Recommendation = () => {
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <p className="p-1">
-                              <small>5 out of 5</small>{" "}
+                              <small>5 out of 5</small>{' '}
                             </p>
-                            <p className="greenText">Patient Ratings</p>
+                            <p className="greenText">Patient list</p>
                             <p className="greenText">Comments</p>
                           </div>
                         </div>
@@ -233,23 +160,25 @@ const Recommendation = () => {
                 <div className="carousel-item">
                   <div className="card">
                     <div className="card-body">
-                      <div className="card-1">
+                      <div className="card-body-inner">
                         <div className="content">
                           <div className="left">
                             <div className="left-img">
-                              <img src={doctor3} width="126px" alt="" />
+                              <img src={doctor} width="126px" alt=""/>
                             </div>
                             <div className="left-text">
                               <h3>
-                                <b>Nicole C. Benkers, PA-C</b>
+                                <b>Alpha</b>
                               </h3>
-                              <p>Cardiology and Physician Assistant</p>
-                              <b>La Crosse Campus</b>
-                              <p> La Crosse, WI</p>
+                              <p>Child Specialist</p>
+                              <h3>
+                                <b>Beta Company</b>
+                              </h3>
+                              <p> Pakistan</p>
                             </div>
                           </div>
                           <div className="right">
-                            <p>(608) 775-2335</p>
+                            <p>+1232123432</p>
                             <p className="greenText">Make appointment</p>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
@@ -257,9 +186,9 @@ const Recommendation = () => {
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <p className="p-1">
-                              <small>5 out of 5</small>{" "}
+                              <small>5 out of 5</small>{' '}
                             </p>
-                            <p className="greenText">Patient Ratings</p>
+                            <p className="greenText">Patient list</p>
                             <p className="greenText">Comments</p>
                           </div>
                         </div>
@@ -276,12 +205,9 @@ const Recommendation = () => {
               data-slide="prev"
             >
               <span
-                style={{
-                  backgroundImage: `url("https://img.icons8.com/ios-filled/50/000000/circled-left-2.png")`
-                }}
                 className="carousel-control-prev-icon"
                 aria-hidden="true"
-              ></span>
+              />
               <span className="sr-only">Previous</span>
             </a>
             <a
@@ -291,12 +217,9 @@ const Recommendation = () => {
               data-slide="next"
             >
               <span
-                style={{
-                  backgroundImage: `url("https://img.icons8.com/ios-filled/50/000000/circled-right-2.png")`
-                }}
                 className="carousel-control-next-icon"
                 aria-hidden="true"
-              ></span>
+              />
               <span className="sr-only">Next</span>
             </a>
           </div>
@@ -306,24 +229,43 @@ const Recommendation = () => {
   );
 };
 
+const RefreshIcon = () => {
+  return (<svg xmlns="http://www.w3.org/2000/svg" 
+           className="ionicon" viewBox="0 0 512 512"><title>Refresh</title><path d="M320 146s24.36-12-64-12a160 160 0 10160 160" fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32"/><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M256 58l80 80-80 80"/></svg>)
+}
+
 function App() {
+
   return (
     <div className="App">
-      <ReactRouterDOM.HashRouter>
-        <Switch>
-          <Route exact path="/">
-            <Main />
+      <div className="search-container-000">
+        <ReactRouterDOM.HashRouter>
+          <Switch>
+            <Route exact path="/">
+              <FormOne/>
+            </Route>
+            <Route path="/speciality">
+              <Link to="/">
+                  <button className="btn btn-lg mb-2 start-again-btn">Start Over <RefreshIcon/>
+                  </button>
+                </Link>
+              <FormTwo/>
+            </Route>
+          </Switch>
+          <Route path="/recommendation">
+            <Link to="/">
+                  <button className="btn btn-lg mb-2 start-again-btn">Start Over <RefreshIcon/>
+                  </button>
+                </Link>
+            <Recommendation/>
           </Route>
-          <Route path="/speciality">
-            <Main1 />
-          </Route>
-        </Switch>
-        <Route path="/recommendation">
-          <Recommendation />
-        </Route>
-      </ReactRouterDOM.HashRouter>
+        </ReactRouterDOM.HashRouter>
+      </div>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
