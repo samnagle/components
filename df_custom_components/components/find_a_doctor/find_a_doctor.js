3e841f0e51a4ee7f7dcc25532ca454e9f13cf08c
Sam Nagle <samnagle@users.noreply.github.com>
2021-05-03 08:34:53 -0700

    Updated bootstrap version

--- df_custom_components/components/find_a_doctor/find_a_doctor.js
+++ df_custom_components/components/find_a_doctor/find_a_doctor.js
@@ -4,6 +4,7 @@ const Link = ReactRouterDOM.Link;
 
 
 
+
 const FormOne = () => {
   return (
     <div className="form-container">
@@ -230,7 +231,7 @@ const Recommendation = () => {
 };
 
 const RefreshIcon = () => {
-  return (<svg xmlns="http://www.w3.org/2000/svg" 
+  return (<svg xmlns="http://www.w3.org/2000/svg"
            className="ionicon" viewBox="0 0 512 512"><title>Refresh</title><path d="M320 146s24.36-12-64-12a160 160 0 10160 160" fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32"/><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M256 58l80 80-80 80"/></svg>)
 }
 
